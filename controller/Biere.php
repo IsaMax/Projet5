1535199f04360f919ed9a20e87123a50f44f388d
Maxime <maxime.isambert@gmail.com>
2020-02-21 12:12:49 +0100

    Premier commit

--- controller/Biere.php
+++ controller/Biere.php
@@ -35,4 +35,65 @@ class Biere {
     public function photo() { return $_photo; }
 
 
+    // Liste des Setters
+
+    public function setIdBrasserie($idBrasserie) {
+
+        $idBrasserie = (int) $idBrasserie;
+
+        if($idBrasserie > 0) {
+            $_idBrasserie = $idBrasserie;
+        }
+    }
+
+
+    public function setNom($nom) {
+
+        $nom = (string) $nom;
+
+        if(strlen($nom) > 0) {
+            $_nom = $nom;
+        }
+    }
+
+    public function setCouleur($couleur) {
+
+        $listeCouleur = Array('blonde', 'speciale', 'brune', 'noire', 'ambree', 'blanche');
+
+        if(in_array($couleur, $listeCouleur)) {
+            $_couleur = $couleur;
+        }
+    }
+
+    public function setDegree($degree) {
+
+        $degree = (int) $degree;
+
+        if($degree > 0 && $degree < 50) {
+            $_degree = $degree;
+        }
+
+    }
+
+    public function setGout($gout) {
+        return $_gout;
+    }
+
+    public function setAlcool() { return $_alcool; }
+
+    public function setMalt() { return $_malt; }
+
+    public function setConservation() { return $_conservation; }
+
+    public function setFiltration() { return $_filtration; }
+
+    public function setType() { return $_type; }
+
+    public function setContenance() { return $_contenance; }
+
+    public function setPresentation() { return $_presentation; }
+
+    public function setPhoto() { return $_photo; }
+
+
 }
